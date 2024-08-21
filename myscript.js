3604827b80112693dbe09ca84d2d41ded842ae02
maxim <maxim@web-vision.de>
2024-08-21 17:03:55 +0200

    will now create a list instead of a div

--- myscript.js
+++ myscript.js
@@ -3,8 +3,11 @@ document.getElementById("submit").addEventListener("click", submit);
 function submit() {
   event.preventDefault();
   let userInput = document.getElementById("inputF");
-  let newDiv = document.createElement("div");
-  newDiv.textContent = userInput.value;
-  document.body.appendChild(newDiv);
-  newDiv.id = "newDiv";
+  let newList = document.createElement("li");
+  newList.textContent = userInput.value;
+  document.body.appendChild(newList);
+  newList.id = "newDiv";
+
+
 }
+
